e76e9dca21aa81d13ede52b9dbf33a7b0afca8b4
François GILLIOEN <francois.gillioen@efrei.net>
2018-11-29 09:35:15 -0500

    removed footer in album viewer

--- testSQL/Web/HTML/UserPage.php
+++ testSQL/Web/HTML/UserPage.php
@@ -505,15 +505,7 @@
       
           </main>
       
-          <footer id="footerAlbum" class="text-muted">
-            <div class="container">
-              <p class="float-right">
-                <a href="#">Back to top</a>
-              </p>
-              <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-              <p>New to Bootstrap? <a href="../../">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
-            </div>
-          </footer>
+         
       
           <!-- Optional JavaScript -->
           <!-- jQuery first, then Popper.js, then Bootstrap JS -->
